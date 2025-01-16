34790fa2942db4613106d1337128f771535240bd
Chacha <vika@blobby.solutions>
2025-01-15 21:08:46 -0800

    Add footer

--- app.vue
+++ app.vue
@@ -1,4 +1,24 @@
 <template>
   <AppHeader />
-  <NuxtPage />
+  <div class="dbm-page-content">
+    <NuxtPage />
+    <div class="dbm-footer">
+      <div class="dbm-logo"></div>
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.dbm-page-content {
+  max-width: var(--app-max-width);
+  margin: 0 auto;
+}
+
+.dbm-logo {
+  background: red;
+  border-radius: 50%;
+  height: 6rem;
+  margin: 0 auto;
+  width: 6rem;
+}
+</style>
