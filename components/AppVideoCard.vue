5e48d8a27a607cf12a174e0555e87e08c50446ad
Chacha <vika@blobby.solutions>
2025-01-16 20:48:28 -0800

    Cut out extra data from AppVideoCard
    Right now I'm just going to treat this as a display unit rather than anything fancy

--- components/AppVideoCard.vue
+++ components/AppVideoCard.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="dbm-video-card">
-    <div class="dbm-video-link"></div>
     <video class="dbm-video-display">
       <source :src="props.src" type="video/mp4" />
-      Hello
     </video>
-    <div class="dbm-video-label">{{ props.title }}</div>
   </div>
 </template>
 
 <script setup lang="ts">
 const props = defineProps<{
   src: string;
-  title: string;
 }>();
 </script>
 
@@ -20,10 +16,6 @@ const props = defineProps<{
 .dbm-video-card {
   aspect-ratio: 1.825/1;
   background: black;
-  border-radius: var(--app-border-radius);
-  margin-top: 1rem;
-  position: relative;
-  overflow: hidden;
   width: 100%;
 }
 
@@ -32,19 +24,5 @@ const props = defineProps<{
   height: 100%;
   margin: 0 auto;
   width: 100%;
-}
-
-.dbm-video-label {
-  background: linear-gradient(0deg, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0) 100%);
-  bottom: 0;
-  left: 0;
-  padding: 4rem 2rem 1rem;
-  position: absolute;
-  width: 100%;
-  z-index: 1;
-
-  @media (max-width: 600px) {
-    padding: 0.5rem 1rem;
-  }
 }
 </style>
