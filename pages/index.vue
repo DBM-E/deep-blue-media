50528b06f0254a87eab008054804c253f9e09e80
Chacha <vika@blobby.solutions>
2025-01-18 09:31:41 -0800

    Put production content back on home page
    I think this would be more straightforward to manage ultimately

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,42 @@
   <div class="dbm-section">
     <a class="dbm-scroll-to" id="services"></a>
     <div class="dbm-section-header">The Services</div>
-    <VideoProductionCard />
+
+    <div class="dbm-video-production-card">
+      <AppVideoCard
+        class="dbm-video-production-display"
+        src="_nuxt/assets/video-production-preview.mp4"
+      />
+      <div class="dbm-video-production-text">
+        <div class="dbm-video-production-section">
+          <div class="dbm-video-production-title">Video Production</div>
+          <div class="dbm-video-production-description">
+            <p>
+              From short-form content for social media and commercial
+              advertisements, to long-form content including weddings,
+              documentaries, and corporate messaging.
+            </p>
+            <p>
+              In addition, we are licensed by the FAA to operate drones—perfect
+              for aerial shots for insurance inspections and real estate
+              listings. We are also SCUBA certified for underwater photography.
+            </p>
+          </div>
+        </div>
+
+        <div class="dbm-video-production-section">
+          <div class="dbm-video-production-title">Video Editing</div>
+          <div class="dbm-video-production-description">
+            <p>
+              We provide standard editing from trimming, splicing and merging to
+              color grading. We can also do basic audio enhancements with music
+              and subtitles.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="dbm-services">
       <AppCard
         class="dbm-service-card"
@@ -56,6 +91,45 @@ const serviceData = [
   @media (min-width: 1024px) {
     grid-template-columns: repeat(4, 1fr);
   }
+}
+.dbm-video-production-card {
+  column-gap: 0.5rem;
+  display: grid;
+  overflow: hidden;
+  row-gap: 0.5rem;
+}
+
+.dbm-video-production-text {
+  column-gap: 0.5rem;
+  display: grid;
+  overflow: hidden;
+  row-gap: 0.5rem;
+
+  @media (min-width: 640px) {
+    grid-template-columns: 1fr 1fr;
+  }
+}
+
+.dbm-video-production-section {
+  background: var(--app-card-color);
+  border: 2px solid var(--app-card-border-color);
+  border-radius: var(--app-border-radius);
+  padding: 1rem;
+
+  @media (min-width: 640px) {
+    padding: 1rem 2rem;
+  }
+}
+
+.dbm-video-production-title {
+  font: 500 1.5rem/1 "Vollkorn", san-serif;
+  padding: 0.5rem 0;
+  text-align: center;
+}
+
+.dbm-video-production-description {
+  margin: 0 auto;
+  max-width: 640px;
 }
 
 .dbm-pricing {
